0a849c9b5150e1638b955b1858165a41c1c85f77
Martijn van Maurik <git@vmaurik.nl>
2019-04-17 14:56:11 +0200

    Try out :-O

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
+import setuptools
+
 from distutils.core import setup
 
 setup(
     name='xfreerdpui',
     version='0.1dev',
-    packages=['xfreerdpui',],
+#    packages=['xfreerdpui',],
+    packages=setuptools.find_packages(),
     scripts=['bin/xfreerdpui'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
