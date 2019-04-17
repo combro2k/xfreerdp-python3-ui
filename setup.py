05e5918953cf6c1a6665361e05a09100bb5089fe
Martijn van Maurik <git@vmaurik.nl>
2019-04-17 14:23:56 +0200

    Try out packaging

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ setup(
     version='0.1dev',
     packages=['xfreerdpui',],
     scripts=['bin/xfreerdpui'],
-    name='xfreerdp-python3-ui',
-    version='0.1dev',
-    packages=['xfreerdp-python3-ui',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
 )
