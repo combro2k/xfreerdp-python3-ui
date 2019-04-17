f9fed39687039ca1de1e9bc50ad4f4005a05fdda
Martijn van Maurik <git@vmaurik.nl>
2019-04-17 14:11:20 +0200

    Try out packaging

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='xfreerdp-python3-ui',
+    version='0.1dev',
+    packages=['xfreerdp-python3-ui',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
