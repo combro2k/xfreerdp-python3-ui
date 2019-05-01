7d6f8298731d2e0d16c220804cb3b7fdc7cd712e
Martijn van Maurik <git@vmaurik.nl>
2019-05-01 11:02:36 +0200

    Fix speed issues to use thinclient alg

--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ from distutils.core import setup
 
 setup(
     name='xfreerdpui',
+    author='Martijn van Maurik',
+    author_email='git@vmaurik.nl',
     version='0.1dev',
     packages=['xfreerdpui',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
+    url='https://github.com/combro2k/xfreerdp-python3-ui/',
     entry_points = {
         'console_scripts': ['xfreerdpui=xfreerdpui.xfreerdpui:main'],
     },
